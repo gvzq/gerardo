a2331f2f5b3d51836f0a201be7d1137dac86c6f8
Gerardo Vazquez <gvzq@hotmail.com>
2022-10-22 17:00:44 -0500

    feat: icons library
    replaced two packages for one that includes many more

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const Flowbite = require('flowbite/plugin');
+
 module.exports = {
   content: [
-    "./node_modules/flowbite-react/**/*.js",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './node_modules/flowbite-react/**/*.js',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("flowbite/plugin")
+    Flowbite,
   ],
-}
+};
