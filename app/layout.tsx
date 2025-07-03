a9517adae3f30d19790ecd8125c91ae6a4b35dc1
Gerardo Vazquez <gvzq@hotmail.com>
2025-07-03 14:45:28 -0500

    Refactor layout component for improved readability and structure
    - Standardized import statements in `layout.tsx` for consistency.
    - Updated metadata object properties to use double quotes.
    - Enhanced clarity of the `clarityMicrosoft` script definition.
    - Modified `RootLayout` component to include a `main` element for better semantic structure and layout styling.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import 'tailwindcss/tailwind.css';
-import Script from 'next/script';
-import React from 'react';
-import { Analytics } from '@vercel/analytics/react';
-import Nav from '@/components/navbar';
-import Footer from '@/components/footer';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "tailwindcss/tailwind.css";
+import Script from "next/script";
+import React from "react";
+import { Analytics } from "@vercel/analytics/react";
+import Nav from "@/components/navbar";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
-  title: 'Gerardo Vazquez',
-  description: 'fCTO and Product',
+  title: "Gerardo Vazquez",
+  description: "fCTO and Product",
 };
-const clarityMicrosoft = '(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "dui4jwiwac");';
+const clarityMicrosoft =
+  '(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "dui4jwiwac");';
 
-import './globals.css'
+import "./globals.css";
 
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
@@ -35,11 +36,11 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="https://gerardo.js.org/manifest/favicon-16x16.png" />
         <link rel="manifest" href="https://gerardo.js.org/manifest/site.webmanifest" />
       </head> */}
-      <body className={inter.variable}>
+      <body className={`${inter.variable} flex flex-col min-h-screen`}>
         <nav>
           <Nav />
         </nav>
-        {children}
+        <main className="flex-grow">{children}</main>
         <footer>
           <Footer />
         </footer>
